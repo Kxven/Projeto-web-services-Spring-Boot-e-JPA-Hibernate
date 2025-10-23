dc5aed7fb1685b812980bbbf0ca082a42cd6eea3
Kxven <kevengodinho3474@gmail.com>
2025-10-22 21:40:18 -0300

    Products

---
+++ src/main/java/com/webproject/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.webproject.course.repositories;
+
+import com.webproject.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
+
